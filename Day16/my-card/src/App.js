a368626a9b291e495e6192e45e77e7474c1fb450
Buddhadeb Chhetri <buddhadebchhetri6@gmail.com>
2021-07-07 19:03:26 +0530

    Day 16

---
+++ Day16/my-card/src/App.js
@@ -0,0 +1,17 @@
+import Header from './Components/Header'
+import "./App.css"
+import logo from "./lmao.jpg"
+
+function App() {
+  return (
+    <div className='container'>
+       <img src={logo} alt="meme" style={{height: '500px', width:'500px' ,border:'5px solid green', borderRadius: '20px'}}/>;
+      <Header />
+       <header className="App-header">
+      
+      </header> 
+    </div>
+  )
+}
+
+export default App;
---
+++ Day16/my-card/src/Components/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <header>
+            {/* <h1>My Task</h1> */}
+        </header>
+    )
+}
+
+export default Header
+
