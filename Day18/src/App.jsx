2e010b89bc3c114dc7380fcdaccde1857ff3b059
Buddhadeb Chhetri <buddhadebchhetri6@gmail.com>
2021-07-09 17:04:59 +0530

    Day18

---
+++ Day18/src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css';
+import Box  from './card';
+function App() {
+  return (
+    
+       <div className="container">
+      <Box/>
+            <Box/><Box/><Box/><Box/><Box/><Box/><Box/><Box/>
+            <Box/><Box/><Box/><Box/><Box/><Box/><Box/><Box/>
+            <Box/><Box/><Box/><Box/><Box/><Box/><Box/><Box/>
+            <Box/><Box/><Box/><Box/><Box/><Box/><Box/><Box/>
+            <Box/><Box/><Box/><Box/><Box/><Box/><Box/><Box/>
+            <Box/><Box/><Box/><Box/><Box/><Box/><Box/><Box/>
+            <Box/><Box/><Box/><Box/><Box/><Box/><Box/><Box/>
+            <Box/><Box/><Box/><Box/><Box/><Box/><Box/><Box/>
+            <Box/><Box/><Box/><Box/><Box/><Box/><Box/><Box/>
+            <Box/><Box/><Box/><Box/><Box/><Box/><Box/><Box/>
+       </div>
+     
+    
+  );
+}
+
+export default App;
---
+++ Day18/src/card.jsx
@@ -0,0 +1,9 @@
+function Card(){
+
+    return(
+        <div className="cards">
+        </div>
+    )
+}
+
+export default Card;
