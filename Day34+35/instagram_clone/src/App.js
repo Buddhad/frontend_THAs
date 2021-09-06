d635b18da8ad271c9da9b741bc2a41569c83c614
Buddhadeb Chhetri <buddhadebchhetri6@gmail.com>
2021-09-06 16:12:20 +0530

    Day35

---
+++ Day34+35/instagram_clone/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Nav from "./Components/Nav";
+import Post from "./Components/Post";
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <Post />
+    </>
+  );
+}
+
+export default App;
